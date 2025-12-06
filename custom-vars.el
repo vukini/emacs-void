f23bc03feb206ecad793d4676194d4cab20a2c52
The Vikid Truth <vukini@gmail.com>
2025-12-06 20:02:20 +0400

    Added Avy package in config.org

--- custom-vars.el
+++ custom-vars.el
@@ -9,12 +9,12 @@
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      default))
  '(package-selected-packages
-   '(cape consult corfu dash-functional embark ess evil evil-collection
-	  exec-path-from-shell go-mode gptel gruber-darker-theme
-	  kind-icon lsp-ivy lsp-mode lsp-ui lua-mode magit marginalia
-	  markdown-mode obsidian orderless pdf-tools php-mode
-	  projectile purescript-mode racket-mode rfc-mode vertico
-	  vterm yasnippet yasnippet-snippets)))
+   '(avy cape consult corfu dash-functional embark ess evil
+	 evil-collection exec-path-from-shell go-mode gptel
+	 gruber-darker-theme kind-icon lsp-ivy lsp-mode lsp-ui
+	 lua-mode magit marginalia markdown-mode obsidian orderless
+	 pdf-tools php-mode projectile purescript-mode racket-mode
+	 rfc-mode vertico vterm yasnippet yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
